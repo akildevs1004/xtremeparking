1ac1b61df4d705258434aae0149cef205a903afe
Venu J <149071174+akildevs1004@users.noreply.github.com>
2025-12-06 15:23:24 +0400

    envconfig

--- frontend/static/config.js
+++
@@ -1,7 +0,0 @@
-window.__APP_CONFIG__ = {
-  apiUrl: "http://127.0.0.1:8000/api",
-  //apiUrl: "https://parkingbackend.xtremeguard.org/api/",
-  PARKING_MODE: false,
-  // PARKING_MODE: false,
-  // PARKING_MODE: false,
-};
