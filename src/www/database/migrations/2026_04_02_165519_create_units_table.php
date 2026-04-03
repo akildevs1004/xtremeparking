b6b6f71e33968198b3e847e8339bdc4baa1b30f9
akildevs1000 <akildevs1000@gmail.com>
2026-04-03 14:07:24 +0400

    added migration for floors and units

---
+++ src/www/database/migrations/2026_04_02_165519_create_units_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('unit_number')->nullable();
+            $table->string('floor_no')->nullable();
+            $table->integer('company_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+};
